efbb190e3810ae2267a756f27a936868aaf0e771
Ziyu Ye <ziyuye@uchicago.edu>
2020-03-14 11:58:38 -0500

    Debug n_features

--- dataset/forecast_dataset.py
+++ dataset/forecast_dataset.py
@@ -26,7 +26,8 @@ def window_data(array, in_size, out_size, overlap=False, n_features=128):
         array_out (np.array): shape is (n_instances, out_size, n_features)
     """
     if len(array.shape) > 2:
-        array = array.reshape(-1, n_features)
+        print(array.shape)
+        array = array.reshape(-1, int(n_features))
 
     len_ = len(array)
     win_size = in_size + out_size
