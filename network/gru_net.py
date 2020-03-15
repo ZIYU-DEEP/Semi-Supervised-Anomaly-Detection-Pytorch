084f7a993c74bcfe44bfd2b49628092b87dceb02
Ziyu Ye <ziyuye@uchicago.edu>
2020-03-14 20:24:47 -0500

    GRU --> LSTM

--- network/gru_net.py
+++ network/gru_net.py
@@ -26,16 +26,15 @@ class GRUNet(BaseNet):
         x = self.act(x)
         return x
 
-
 class GRUNetStacked(BaseNet):
     def __init__(self):
         super().__init__()
 
-        self.gru1 = nn.GRU(128, 64, num_layers=1, batch_first=True)
+        self.gru1 = nn.LSTM(128, 64, num_layers=1, batch_first=True)
         self.BN1 = nn.BatchNorm1d(100)
-        self.gru2 = nn.GRU(64, 64, num_layers=1, batch_first=True)
+        self.gru2 = nn.LSTM(64, 64, num_layers=1, batch_first=True)
         self.BN2 = nn.BatchNorm1d(100)
-        self.gru3 = nn.GRU(64, 64, num_layers=1, batch_first=True)
+        self.gru3 = nn.LSTM(64, 64, num_layers=1, batch_first=True)
         self.BN3 = nn.BatchNorm1d(100)
         self.fc = nn.Linear(64 * 100, 128 * 25)
         self.act = nn.ReLU()
@@ -52,3 +51,29 @@ class GRUNetStacked(BaseNet):
         x = x.reshape(-1, 25, 128).contiguous()
         x = self.act(x)
         return x
+
+# class GRUNetStacked(BaseNet):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.gru1 = nn.GRU(128, 64, num_layers=1, batch_first=True)
+#         self.BN1 = nn.BatchNorm1d(100)
+#         self.gru2 = nn.GRU(64, 64, num_layers=1, batch_first=True)
+#         self.BN2 = nn.BatchNorm1d(100)
+#         self.gru3 = nn.GRU(64, 64, num_layers=1, batch_first=True)
+#         self.BN3 = nn.BatchNorm1d(100)
+#         self.fc = nn.Linear(64 * 100, 128 * 25)
+#         self.act = nn.ReLU()
+#
+#     def forward(self, x):
+#         x, _ = self.gru1(x)
+#         x = self.BN1(x)
+#         x, _ = self.gru2(x)
+#         x = self.BN2(x)
+#         x, _ = self.gru3(x)
+#         x = self.BN3(x)
+#         x = x.reshape(x.size(0), 64 * 100).contiguous()
+#         x = self.fc(x)
+#         x = x.reshape(-1, 25, 128).contiguous()
+#         x = self.act(x)
+#         return x
