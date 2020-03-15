90ef6ebc81f2ae25b0f1ba0db59dacfb82ad23f5
Ziyu Ye <ziyuye@uchicago.edu>
2020-03-14 22:12:35 -0500

    try new lstm

--- network/gru_net.py
+++ network/gru_net.py
@@ -30,26 +30,26 @@ class GRUNetStacked(BaseNet):
     def __init__(self):
         super().__init__()
 
-        self.gru1 = nn.LSTM(128, 64, num_layers=1, batch_first=True)
+        self.lstm1 = nn.LSTM(128, 64, num_layers=3, batch_first=True)
         self.BN1 = nn.BatchNorm1d(100)
-        self.gru2 = nn.LSTM(64, 64, num_layers=1, batch_first=True)
-        self.BN2 = nn.BatchNorm1d(100)
-        self.gru3 = nn.LSTM(64, 64, num_layers=1, batch_first=True)
-        self.BN3 = nn.BatchNorm1d(100)
+        # self.lstm2 = nn.LSTM(64, 64, num_layers=1, batch_first=True)
+        # self.BN2 = nn.BatchNorm1d(100)
+        # self.lstm3 = nn.LSTM(64, 64, num_layers=1, batch_first=True)
+        # self.BN3 = nn.BatchNorm1d(100)
         self.fc = nn.Linear(64 * 100, 128 * 25)
-        self.act = nn.ReLU()
+        # self.act = nn.ReLU()
 
     def forward(self, x):
-        x, _ = self.gru1(x)
+        x, _ = self.lstm1(x)
         x = self.BN1(x)
-        x, _ = self.gru2(x)
-        x = self.BN2(x)
-        x, _ = self.gru3(x)
-        x = self.BN3(x)
+        # x, _ = self.lstm2(x)
+        # x = self.BN2(x)
+        # x, _ = self.lstm3(x)
+        # x = self.BN3(x)
         x = x.reshape(x.size(0), 64 * 100).contiguous()
         x = self.fc(x)
         x = x.reshape(-1, 25, 128).contiguous()
-        x = self.act(x)
+        # x = self.act(x)
         return x
 
 # class GRUNetStacked(BaseNet):
