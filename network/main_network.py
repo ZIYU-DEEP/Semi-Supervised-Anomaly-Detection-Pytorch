6b49cb53e531a778b8e10cc232b1caf0cc3fe74f
Ziyu Ye <ziyuye@uchicago.edu>
2020-03-16 09:03:28 -0500

    Update main_network.py

--- network/main_network.py
+++ network/main_network.py
@@ -8,16 +8,16 @@ Reference: https://github.com/lukasruff/Deep-SAD-PyTorch/tree/master/src/network
 from gru_net import GRUNet, GRUNetStacked
 
 
-def build_network(net_name='gru'):
-    known_networks = ('gru', 'gru_stacked')
+def build_network(net_name='lstm'):
+    known_networks = ('lstm', 'lstm_stacked')
     assert net_name in known_networks
 
     net_name = net_name.strip()
 
-    if net_name == 'gru':
-        return GRUNet()
+    if net_name == 'lstm':
+        return LSTMNet()
 
-    if net_name == 'gru_stacked':
-        return GRUNetStacked()
+    if net_name == 'lstm_stacked':
+        return LSTMNetStacked()
 
     return None
