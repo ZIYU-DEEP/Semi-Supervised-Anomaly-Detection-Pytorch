bb52064164b502e001cf7f3055e0fcc671d50e13
Ziyu Ye <ziyuye@uchicago.edu>
2020-03-18 22:51:24 -0500

    Done

--- utils/save_abnormal_xs.py
+++ utils/save_abnormal_xs.py
@@ -5,12 +5,32 @@ Author: Lek'Sai Ye, University of Chicago
 """
 
 import argparse
+from pathlib import Path
 from utils import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='forecast', help='[Choice] forecast, deepsad')
 parser.add_argument('--normal_folder', type=str, default='ryerson')
 parser.add_argument('--abnormal_folder', type=str, default='downtown_LOS-5M-USRP1')
-parser.add_argument('--window_size', type=int, default=100)
+parser.add_argument('--window_size', type=int, default=100, help='Do not try 1000. GPU will die.')
 parser.add_argument('--predict_size', type=int, default=25)
-parser.add_argument('--n_features', type=int, default=128) 
+parser.add_argument('--n_features', type=int, default=128)
+p = parser.parse_args()
+
+model, normal_folder, abnormal_folder = p.model, p.normal_folder, p.abnormal_folder
+
+if model == 'deepsad':
+    root = 'net/adv_spectrum/data/feature/downsample_10/abnormal'
+    root_ = '/net/adv_spectrum/torch_data_deepsad'
+    path = '{}/{}/{}_{}'.format(root, abnormal_folder, window_size, predict_size)
+    path_ = '{}/{}/{}/abnormal/{}'.format(root_, window_size, normal_folder, abnormal_folder)
+    save_abnormal_Xs_deepsad(path, path_, window_size, n_features)
+
+elif model == 'forecast':
+    root = 'net/adv_spectrum/data/feature/downsample_10/abnormal'
+    root_ = '/net/adv_spectrum/torch_data'
+    path = '{}/{}/{}_{}'.format(root, abnormal_folder, window_size, predict_size)
+    path_ = '{}/{}/abnormal/{}'.format(root_, normal_folder, abnormal_folder)
+    save_abnormal_Xs(path, path_, window_size, n_features)
+
+print('Done. Got to go to bed. Bye.')
